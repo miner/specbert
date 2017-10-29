05c2ddec7bcc6902671fc5768a63c08909c1a82c
Steve Miner <steveminer@gmail.com>
2017-10-29 10:38:54 -0400

    beta3

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/miner/specbert"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta3"]
                  [org.clojure/spec.alpha "0.1.134"]
                  [org.clojure/test.check "0.10.0-alpha2"]
                  [com.velisco/herbert "0.7.0"]
