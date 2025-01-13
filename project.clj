9c5ce8adbce4cc827c83e8dd6094e534263b1373
Steve Miner <steveminer@gmail.com>
2025-01-13 10:33:18 -0500

    clj1.12

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/miner/specbert"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.4"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.clojure/alpha.spec "0.2.177-SNAPSHOT"]
                  [org.clojure/test.check "1.1.1"]
                  [com.velisco/herbert "0.7.0"]
